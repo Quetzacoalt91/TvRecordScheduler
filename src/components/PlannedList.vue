2ca0e21c7fc7a007c0468c1cfad899b6146db4d1
Thomas Nabord <thomas.nabord@prestashop.com>
2018-09-09 18:43:18 +0100

    Initial commit

---
+++ src/components/PlannedList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h2>Programmed recordings</h2>
+    <ul>
+      <li v-for="(program, index) in programs" v-bind:key="index">
+        {{ program.channel }} - From {{ program.from }} to {{ program.to }}
+        <button class="btn btn-sm btn-outline-danger" v-on:click="deleteProgram(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlannedList',
+  computed: {
+    programs() {
+      return this.$store.state.programs;
+    },
+  },
+  methods: {
+    deleteProgram(index) {
+      this.$store.commit(
+        'deleteProgram', {
+          index,
+        },
+      );
+    },
+  },
+};
+</script>
+
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+export default {
+  state: {
+    programs: [
+      {
+        channel: 'BBC One',
+        from: '15:00',
+        to: '16:30',
+      },
+      {
+        channel: 'E4',
+        from: '20:02',
+        to: '17:00',
+      },
+    ],
+  },
+  mutations: {
+    deleteProgram(state, payload) {
+      state.programs.splice(payload.index, 1);
+    },
+  },
+};
